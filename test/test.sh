1b3d49fcc5b3c95141b2a2193c13968a72567232
mak3551 <nawuoil@yahoo.co.jp>
2025-01-15 22:17:08 +0900

    テストコード微修正

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "${0}")" || exit 1
 ../gunzip_demo sample.txt.gz sample_output || exit 1
 md5sum -c --quiet MD5SUM || { rm -f sample_output; exit 1; }
-rm -f sample_output
+rm -f sample_output || exit 1
