d9db6c77ac67c24dd23ed9763deb4e11e65eb615
mak3551 <nawuoil@yahoo.co.jp>
2025-01-17 19:19:08 +0900

    テスト時に結果を出力するようにした

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd "$(dirname "${0}")" || exit 1
-../gunzip_demo sample.txt.gz sample_output || exit 1
-md5sum -c --quiet MD5SUM || { rm -f sample_output; exit 1; }
-rm -f sample_output || exit 1
+cd "$(dirname "${0}")" || { echo "Test failed."; exit 1; }
+../gunzip_demo sample.txt.gz sample_output || { echo "Test failed."; exit 1; }
+md5sum -c --quiet MD5SUM || { rm -f sample_output; echo "Test failed."; exit 1; }
+rm -f sample_output || { echo "Test failed."; exit 1; }
+echo "Test succeeded."
+exit 0
